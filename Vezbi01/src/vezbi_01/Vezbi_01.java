7a247ca4fc100ab24f4b4061497f698d8aa4e52d
Nonsata77 <127878155+Nonsata77@users.noreply.github.com>
2023-11-05 17:50:57 +0100

    Add files via upload

---
+++ Vezbi01/src/vezbi_01/Vezbi_01.java
@@ -0,0 +1,10 @@
+package vezbi_01;
+
+public class Vezbi_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Hello World OOP FIKT");
+	}
+
+}
